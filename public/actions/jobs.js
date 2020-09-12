de071a39023282d5e782e8f8738c922d241f9caa
Andrés Saavedra <hola@and318.com>
2020-09-11 22:58:40 -0300

    fix modal

--- public/actions/jobs.js
+++ public/actions/jobs.js
@@ -40,6 +40,7 @@ function clean() {
             $('#lista_jobs').empty();
             response.data.forEach((job,index) => {
                 listJob(job,index);
+                printModal(job,index);
             })
             console.log(response);
         } catch (error) {
@@ -53,66 +54,56 @@ function imgError(image) {
     return true;
 }
 
+
 function listJob(job,index) {
 
 
     var content = `
-    
-    
     <div class="card mb-3 ml-3" >
-                    <div class="row no-gutters">
-                        <div class="col-md-4 d-flex justify-content-center align-items-center">
-                            <img src="${job.company_logo}" onerror="imgError(this);" class="card-img" alt="logo">
-                        </div>
-                        <div class="col-md-8">
-                            <div class="card-body">
-                                <h5 class="card-title">${job.title}</h5>
-                                <span>${job.location}</span> <span>${job.type}</span>
-                                <button type="button" class="btn btn-outline-success btn-sm" data-toggle="modal" data-target="#${index}">
-                                Ver más
-                                </button>
-                                <div class="modal fade" id="${index}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="MdalLabel">Descripción del puesto</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <small>
-                                    ${job.description}
-                                    </small>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
-                               
-                                
-                                <div class="collapse" id="${job.id}">
-                                    <small>
-                                    ${job.description}
-                                    </small>
-                                </div>
-                                <p class="card-text reduce"><small>sss</small></p>
-                                <p class="card-text"><small class="text-muted">${job.created_at}</small></p>
-                            </div>
-                        </div>
-                    </div>
+    <div class="row no-gutters">
+        <div class="col-md-4 d-flex justify-content-center align-items-center">
+            <img src="${job.company_logo}" onerror="imgError(this);" class="card-img" alt="logo">
+        </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title">${job.title}</h5>
+                    <span>${job.location}</span> <span>${job.type}</span>
+                    <button type="button" class="btn btn-outline-primary btn-small" data-toggle="modal" data-target="#asdf${index}">
+  Ver más
+</button>
+                    <p class="card-text reduce"><small><a href="${job.company_url}">${job.company_url}</a></small></p>
+                    <p class="card-text"><small class="text-muted">${job.created_at}</small></p>
                 </div>
 
-    
-    
-    
-    
-    
-    
+            </div>
+        </div>
+</div>
     `
     $(`#lista_jobs`).append(content);
 }
+function printModal(job,index) {
 
+    var modal = `   
+     <div class="modal fade" id="asdf${index}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-scrollable modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="ModalLabel">Descripción del puesto</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <small>
+                    ${job.description}
+                </small>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+            </div>
+        </div>
+    </div>
+</div>
+`
+$(`#modal_container`).append(modal);
+}
